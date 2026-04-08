dc90c904e110c6db69dbd220ec76da735ffb345d
Liquid <yyj060313@gmail.com>
2026-04-08 09:44:55 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 836 ms, Memory: 134932 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -0,0 +1,17 @@
+import sys
+import bisect
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().split()))
+
+result = [a[0]]
+
+for i in range(1, n):
+    if a[i] > result[-1]:
+        result.append(a[i])
+    else:
+        low_num = bisect.bisect_left(result, a[i])
+        result[low_num] = a[i]
+
+print(len(result))
