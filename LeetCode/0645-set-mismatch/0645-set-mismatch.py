08adb019eb15b52e862c26fc2d313e38a19f6c11
GitHub Action <action@github.com>
2026-05-04 00:41:22 +0000

    자동 정리: LeetCode 문제 폴더 이동

--- LeetCode/0645-set-mismatch/0645-set-mismatch.py
+++ LeetCode/0645-set-mismatch/0645-set-mismatch.py
@@ -1,15 +1,15 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        a = []
-        b = []
-        x = [i for i in range(1, len(nums) + 1)]
-        for i in range(len(nums)):
-            if nums[i] in a:
-                b.append(nums[i])
-            elif nums[i] not in a:
-                a.append(nums[i])
-        for i in range(len(x)):
-            if x[i] not in a:
-                b.append(x[i])
-
-        return b
+        seen = set()
+        duplicate = -1
+        
+        for num in nums:
+            if num in seen:
+                duplicate = num
+            else:
+                seen.add(num)
+        
+        n = len(nums)
+        missing = (set(range(1, n + 1)) - seen).pop()
+        
+        return [duplicate, missing]
