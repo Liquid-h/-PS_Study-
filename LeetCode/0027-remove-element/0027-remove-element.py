9d2552bc7ab8fadf33c30a05c51e1f7f2cd56541
GitHub Action <action@github.com>
2026-05-06 02:57:12 +0000

    자동 정리: LeetCode 문제 폴더 이동

--- LeetCode/0027-remove-element/0027-remove-element.py
+++ LeetCode/0027-remove-element/0027-remove-element.py
@@ -1,8 +1,9 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        result = []
+        k = 0
+        
         for i in range(0, len(nums)):
             if nums[i] != val:
-                result.append(nums[i])
-        nums[:] = result
-        return len(nums)
+                nums[k] = nums[i]
+                k += 1
+        return k
