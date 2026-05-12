1e486894b430850852b92de9953b22505b48e9e3
Liquid <yyj060313@gmail.com>
2026-05-12 14:58:56 +0900

    Time: 0 ms (100%), Space: 20.6 MB (27.68%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -0,0 +1,17 @@
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        result = None
+        while head:
+            next_node = head.next
+            head.next = result
+            result = head
+            head = next_node
+        return result
+    
+    
